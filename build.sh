acab917eafaeff39f9c4e531de638825fb3ed778
stelzo <stelzo>
2020-06-24 01:03:03 +0200

    g++ with include dir

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 mkdir build && cd build
-cmake -DCMAKE_BUILD_TYPE=Debug -DENABLE_TESTING=ON -DCMAKE_CXX_COMPILER=gcc ..
+cmake -DCMAKE_BUILD_TYPE=Debug -DENABLE_TESTING=ON -DCMAKE_CXX_COMPILER=g++ ..
 make
